a197794ae0e174356fa3b86afd067395a5aa019c
Luis Cobián <coby10@gmail.com>
2018-12-05 23:53:29 -0600

    Initial commit

---
+++ src/com/function/Graph.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.function;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author luiscobian
+ */
+public class Graph {
+    
+    private Set<Node> nodes = new HashSet<>();
+     
+    public void addNode(Node nodeA) {
+        nodes.add(nodeA);
+    }
+
+    public Set<Node> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(Set<Node> nodes) {
+        this.nodes = nodes;
+    }
+    
+    
+}
